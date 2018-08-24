a997ad8f2a8dbc9a1725d56ee678f6918bb4a3d2
Scarlett <scar.antoine@gmail.com>
2018-08-24 18:45:57 -0300

    Se aplica la funcion para extraer links

--- src/md-links.js
+++ src/md-links.js
@@ -1,7 +1,7 @@
 
 const path = require('path');
 const fs = require('fs');
-const marked = require('marked');
+const Marked = require('marked');
 
 
 function rutaAbsoluta(ruta) {
@@ -14,7 +14,8 @@ function rutaAbsoluta(ruta) {
 function leerRutacompleta(ruta2) {
   fs.readFile(ruta2, 'utf-8', (err, data)=> {
     if (err) throw err;
-    console.log(data);
+    markdownLinkExtractor(data);
+    // console.log(data);
   });
 }
 
@@ -28,6 +29,42 @@ function comprueboExtencion(ruta3) {
     console.log('error esto no es .md');
   }
 }
+
+
+function markdownLinkExtractor(markdown) {
+  const links = [];
+  const renderer = new Marked.Renderer();
+
+  const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
+
+  Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
+
+  renderer.link = function(href, title, text) {
+    links.push({
+      href: href,
+      text: text,
+      title: title,
+    });
+  };
+
+  renderer.image = function(href, title, text) {
+    // Remove image size at the end, e.g. ' =20%x50'
+    href = href.replace(/ =\d*%?x\d*%?$/, '');
+    links.push({
+      href: href,
+      text: text,
+      title: title,
+    });
+  };
+
+  Marked(markdown, {renderer: renderer});
+  console.log(links);
+  
+  // return links;
+};
+
 
 module.exports = {
   rutaAbsoluta,
