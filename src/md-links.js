fc0b12c073bf31a081e318f0b3dc4471587d2a28
Scarlett <scar.antoine@gmail.com>
2018-08-24 16:31:16 -0300

    Funcion para leer readme.

--- src/md-links.js
+++ src/md-links.js
@@ -1,10 +1,17 @@
 // prueba para realizar test con jest
+const path = require('path');
+const fs = require('fs');
+
 
 function rutaAbsoluta(ruta) {
-  console.log(path.resolve(ruta));
+  const rutaCompleta = path.resolve(ruta);
+  //console.log(rutaCompleta);
+  fs.readFile(rutaCompleta, 'utf-8', (err, data)=> {
+    if (err) throw err;
+    console.log(data);
+  });
 }
-
-
+ 
 module.exports = {
-  rutaAbsoluta
+  rutaAbsoluta,
 };
